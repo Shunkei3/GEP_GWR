d1f96a74fffac83350af827a92e8742167174275
Shunkei <kakim002@umn.edu>
2025-10-18 20:46:00 -0500

    code for preprocessing the raw data

---
+++ Code/InVEST_SWY/x_PrepData/Prep_CostData.R
@@ -0,0 +1,13 @@
+# /*===========================================*/
+#'= Objective =
+#' + 
+# /*===========================================*/
+# /*===== Setup =====*/
+library(data.table)
+library(dplyr)
+
+data_dir <- "/Volumes/baseHD/NatCapTEEMs/GEP_old/GWR/Data"
+
+elec_price_raw_dt <- 
+  fread(file.path())
+
